801fa924ad3a9c85a4fc80e6fb75e61a9e4cea70
amj133 <amj@vt.edu>
2018-03-19 15:10:17 -0600

    create /logout path and destroy action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
 
   def create
-    render text: request.env["omniauth.auth"].inspect
-    binding.pry
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    session.destroy
+
+    redirect_to root_path
   end
 
 end
