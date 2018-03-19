a8dd9d22d4baad4db7ea40862bf5a891dbd4c302
amj133 <amj@vt.edu>
2018-03-19 15:15:59 -0600

    add mock_auth to users_login_with_github_spec

--- spec/features/user_logs_in_with_github_spec.rb
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe "user logs in" do
   it "using github oauth" do
+    omniauth_stub
     visit root_path
 
     expect(page).to have_link("Login with GitHub")
 
     click_on("Login with GitHub")
-    omniauth_stub
 
-    expect(page).to have_content("Welcome Andrew Jeffery")
-    expect(page).to have_link("Logout")
+    expect(page).to have_content("Welcome amj133")
+    expect(page).to have_link("logout")
   end
 
   def omniauth_stub
@@ -18,7 +18,15 @@ describe "user logs in" do
 
     OmniAuth.config.mock_auth[:github] =
       OmniAuth::AuthHash.new({
-                              # empty for now
+                              "provider" => "github",
+                              "uid" => 31484552,
+                              "info" =>
+                              {"email" => "amj@vt.edu", "urls" => {"GitHub" => "https://github.com/amj133"}},
+                              "credentials" => {"token" => "bb9520046738fe2eb647a037ef5065cb6eebfd40", "expires" => false},
+                              "extra" =>
+                              {"raw_info" =>
+                                   {"login" => "amj133",
+                                    "id" => 31484552}}
                               })
   end
 end
