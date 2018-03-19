b618f738dec368b0ed216da62ad777acbe522fe3
amj133 <amj@vt.edu>
2018-03-19 13:58:05 -0600

    create user model with uid login email oauth_token oauth_token_secret

---
+++ db/migrate/20180319195721_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :uid
+      t.string :login
+      t.string :email
+      t.string :oauth_token
+      t.string :oauth_token_secret
+
+      t.timestamps
+    end
+  end
+end
