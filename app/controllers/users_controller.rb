e0d40863e909b6505c11ac133c60bab08e989ff0
amj133 <amj@vt.edu>
2018-03-20 15:43:01 -0600

    create repository model - repo spec passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,8 @@ class UsersController < ApplicationController
     conn = Faraday.new(:url => "https://api.github.com/users/#{current_user.login}/repos", headers: {"access_token" => current_user.oauth_token})
     response = conn.get
     @repos = JSON.parse(response.body).map do |raw_repo|
-      # raw_repo['name']
       Repository.new(raw_repo['name'])
     end
-  end
-
-end
-
-class Repository
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
   end
 
 end
