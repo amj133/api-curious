b9be6c0949b5656f0f28c52b6f8ddd10500f6964
amj133 <amj@vt.edu>
2018-03-19 18:10:12 -0600

    create users controller and users show actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  resources :users, only: [:show]
+
   get "/auth/github", as: :github_login
   get "/auth/github/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
