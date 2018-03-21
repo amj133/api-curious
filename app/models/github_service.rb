a4f138834ec1566699c4ba4337121239eafb3d71
amj133 <amj@vt.edu>
2018-03-21 06:58:03 -0600

    create #find_followers_and_commits method in github_service model - not yet tested

--- app/models/github_service.rb
+++ app/models/github_service.rb
@@ -6,6 +6,7 @@ class GithubService
       faraday.headers["access_token"] = user.oauth_token
       faraday.adapter Faraday.default_adapter
     end
+    @date_limit = (Date.today - 14).strftime('%Y-%m-%d')
   end
 
   def get_url(url)
@@ -22,7 +23,7 @@ class GithubService
 
   def find_followers
     response = get_url("https://api.github.com/users/#{@user.login}/followers")
-    response.map do |follower|
+    @user_followers = response.map do |follower|
       Follower.new(follower[:login])
     end
   end
@@ -41,11 +42,9 @@ class GithubService
     end
   end
 
-  def find_recent_commits
-    date = (Date.today - 14).strftime('%Y-%m-%d')
-
+  def find_recent_commits(username = @user.login)
     response = @conn.get do |req|
-      req.url "/search/commits?q=author-date:>#{date} author:#{@user.login}"
+      req.url "/search/commits?q=author-date:>#{@date_limit} author:#{username}"
       req.headers['Accept'] = "application/vnd.github.cloak-preview+json"
     end
 
@@ -55,6 +54,14 @@ class GithubService
       commits << Commit.new(response[:items][i][:repository][:name], response[:items][i][:url])
     end
     commits
+  end
+
+  def find_followers_commits
+    followers_and_commits = {}
+    @user_followers.each do |follower|
+      followers_and_commits[follower] = find_recent_commits(follower.username)
+    end
+    followers_and_commits
   end
 
   # https://api.github.com/search/commits/q=author-date:>2018-03-06 author:amj133
