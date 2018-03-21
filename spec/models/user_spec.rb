e2cbbda9b26995d3637f0484d2a458ea0025a648
amj133 <amj@vt.edu>
2018-03-21 15:28:41 -0600

    update user_spec per github_user model creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:auth) {
+    {
+      "uid" => 15,
+      "info" => { "email" => "monkeysrule@gmail.com", "urls" => {"GitHub" => "https://github.com/amj133"}},
+      "credentials" => {"token" => "12345", "expires" => false},
+      "extra" =>
+        {"raw_info" =>
+          {"login" => "monkeysrule",
+           "avatar_url" => "https://monkeysrule.com",
+           "id" => 31484552,
+           "name" => "Monkey Man",
+           "location" => "Denver, CO",
+           "bio" => "My mother was a wildlife biologist and I love monkeys!"}}
+    }
+  }
+
   it "can be created from oauth hash" do
-    auth = {"provider" => "github",
-            "uid" => 31484552,
-            "info" =>
-              {"email"=>"amj@vt.edu", "urls" => {"GitHub" => "https://github.com/amj133"}},
-            "credentials" => {"token" => "bb9520046738fe2eb647a037ef5065cb6eebfd40", "expires" => false},
-            "extra" =>
-               {"raw_info" =>
-                 {"login" => "amj133",
-                  "avatar_url" => "https://avatars0.githubusercontent.com/u/31484552?v=4",
-                  "id" => 31484552}}}
+    # auth = {"provider" => "github",
+    #         "uid" => 31484552,
+    #         "info" =>
+    #           {"email"=>"amj@vt.edu", "urls" => {"GitHub" => "https://github.com/amj133"}},
+    #         "credentials" => {"token" => "bb9520046738fe2eb647a037ef5065cb6eebfd40", "expires" => false},
+    #         "extra" =>
+    #            {"raw_info" =>
+    #              {"login" => "amj133",
+    #               "avatar_url" => "https://avatars0.githubusercontent.com/u/31484552?v=4",
+    #               "id" => 31484552}}}
 
     user = User.from_omniauth(auth)
 
-    expect(user.uid).to eq("31484552")
-    expect(user.login).to eq("amj133")
-    expect(user.email).to eq("amj@vt.edu")
-    expect(user.oauth_token).to eq("bb9520046738fe2eb647a037ef5065cb6eebfd40")
+    expect(user.uid).to eq("15")
+    expect(user.oauth_token).to eq("12345")
   end
 
   describe "class methods" do
